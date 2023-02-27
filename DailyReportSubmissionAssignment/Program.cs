af5661b83167ee8e82863f06002cb2a8c17f2379
markedin <119455170+markedin@users.noreply.github.com>
2023-02-27 15:22:29 -0800

    This push includes the Daily Report Submission assignment. It is a console app that mimics TTA's daily reports.

---
+++ DailyReportSubmissionAssignment/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "";
+            string myCourse = "";
+
+            int myPageInt = 0;
+            string myPage = "";
+
+            bool myHelp = false;
+            string myHelpString = "";
+
+            string myExperiences = "";
+
+            string myFeedback = "";
+
+            byte myHours = 0;
+            string myHoursString = "";
+
+
+
+            //This block of code writes the opening statements of the program
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            
+            //This block asks for your name, and stores it in myName
+            Console.WriteLine("What is your name?");
+            myName = Console.ReadLine();
+
+            //This block asks what course youre on, and stores it in myCourse
+            Console.WriteLine("What course are you on?");
+            myCourse = Console.ReadLine();
+
+            //This block asks what page number, and stores it in myPage, which gets 
+            //converted to an Int32, and then is stored as an int in myPageInt
+            Console.WriteLine("What page number?");
+            myPage = Console.ReadLine();
+            myPageInt = Convert.ToInt32(myPage);
+
+            //This block asks if you need help, and stores your response in myHelpString,
+            //which is then converted to a boolean value stored in myHelp
+            Console.WriteLine("Do you need help with anything? Please " +
+                "answer 'true' or 'false'.");
+            myHelpString = Console.ReadLine();
+            myHelp = Convert.ToBoolean(myHelpString);
+
+            //This block asks if there were any positive experiences, and the stores 
+            //the response in myFeedback
+            Console.WriteLine("Were there any positive experiences you'd like" +
+                " to provide? Please be specific.");
+            myFeedback = Console.ReadLine();
+
+            //This block asks how many hours of study you had and stores it in myHoursString
+            //, which is then converted from string to Byte and is stored in myHours
+            Console.WriteLine("How many hours did you study today?");
+            myHoursString = Console.ReadLine();
+            myHours = Convert.ToByte(myHoursString);
+
+
+            //This block writes some text that should appear at the end of the program. 
+            Console.WriteLine("Thank you for your answers. An instructor will respond" +
+                "to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("what is your name?");
+            //string yourname = Console.ReadLine();
+            //Console.WriteLine("your name is: " + yourname);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite Number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+            double heightInCentimeters = 211.3204923402;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Mark";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+        }
+    }
+}
