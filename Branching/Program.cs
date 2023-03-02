f066294223ac6e5535526452114a4568fd241e58
markedin <119455170+markedin@users.noreply.github.com>
2023-03-02 14:49:21 -0800

    This project is for TTA's branching submission assignment. It is a mock up of a shipping center application.

---
+++ Branching/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int currentTemperature = 70;
+            //int roomTemperature = 70;
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("it is not exactly room temp");
+            //}
+
+            //Console.ReadLine();
+
+            int roomTemperature = 70;
+
+            Console.WriteLine("Hi what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + ", what is the temp where youre at?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            if (currentTemperature == roomTemperature) 
+            {
+                Console.WriteLine("It is exactly room temp.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temp");
+            }
+            else if (currentTemperature < roomTemperature)
+            {
+                Console.WriteLine("It is colder than room temp");
+            }
+            else
+            {
+                Console.WriteLine("Uhhhh no way homie");
+            }
+        }
+    }
+}
---
+++ BranchingSubmissionAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. PLease follow the instructions below");
+
+            Console.WriteLine("Please enter the package weight");
+            int myWeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package width:");
+            int myWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height: ");
+            int myHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int myLength = Convert.ToInt32(Console.ReadLine());
+
+            if (myLength <= 50 && myWidth <= 50 && myHeight <= 50)
+            {
+                decimal myPrice = (myHeight * myWidth * myLength * myWeight) / 100;
+                Console.WriteLine("Your estimated total for shipping this package is : " + "$" + myPrice + ".00");
+                Console.WriteLine("Thank you!");
+               
+
+            }
+
+            else
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
