7ff8b40e70c3a25787c4046106164b9a966ebafc
markedin <119455170+markedin@users.noreply.github.com>
2023-03-13 11:30:26 -0700

    This push includes MainMethodSubmissionAssignment

---
+++ MainMethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    public class MathOperations
+    {
+        //Create a method that takes in an int, performs a math operation on it, and returns 
+        //the answer as an int
+        public static int maths(int integerIN)
+        {
+            //create a variable to return to the main program
+            int intOut1 = integerIN + 9;
+            //return the answer post math operation to the main program
+            return intOut1;
+        }
+
+        //Create a method (with the same name) that takes in a decimal, performs a math operation
+        //on it, and returns the asnwer as an integer
+        public static int maths(decimal integerIN)
+        {
+            //create a var and a math operation to return to the main program
+            int intOut2 = Convert.ToInt32(integerIN * 2);
+            //return the answer as an int to the main program
+            return intOut2;
+        }
+        //create a method (with the same name) that will take in a string, convert to an int if possible
+        //and will do a math operation on it. then, return as int
+        public static int maths(string stringIN) 
+        {
+            //create a var and try to convert string input to int
+            int intOut3 = Convert.ToInt32(stringIN);
+            //perform math operation on intOut3
+            intOut3 = intOut3 * 3 + 2;
+            //return intOut3 as int
+            return intOut3;
+
+        }
+    }
+}
---
+++ MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //define variables that will be used to hold the return value of all 3 methods
+            int integerOut1 = 0;
+            int integerOut2 = 0;
+            int integerOut3 = 0;
+
+            //instantiate class1 and call first method, passing in an int
+            integerOut1 = MathOperations.maths(3);
+            //Write strings to console showing what decimal number was passed in to method, what the answer was,
+            //and what data type the answer was returned as
+            Console.WriteLine("Passing in an integer with a value of 3 to the first method.");
+            Console.WriteLine("The result is: " + integerOut1 + " with a data type of: " + integerOut1.GetType() + "\n");
+
+            //instantiate class1 and call second method, passing in a decimal
+            integerOut2 = MathOperations.maths(1.110439m);
+            //Write strings to console showing what decimal number was passed in to method, what the answer was,
+            //and what data type the answer was returned as
+            Console.WriteLine("Passing in a decimal with a value of 1.110439 to the second method.");
+            Console.WriteLine("The result is: " + integerOut2 + " with a data type of: " + integerOut2.GetType() + "\n");
+
+            //instantiate class1 and call first method, passing in a string that equates to an int
+            integerOut3 = MathOperations.maths("3");
+            //Write strings to console showing what decimal number was passed in to method, what the answer was,
+            //and what data type the answer was returned as
+            Console.WriteLine("Passing in a string with a value of \"3\" to the third method.");
+            Console.WriteLine("The result is: " + integerOut3 + " with a data type of: " + integerOut3.GetType() + "\n");
+            Console.ReadLine();
+        }
+    }
+}
