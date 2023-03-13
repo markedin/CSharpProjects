219c74f6d6d3cb1420ac320905be53b2ad54388c
markedin <119455170+markedin@users.noreply.github.com>
2023-03-13 14:07:40 -0700

    This push includes MethodSubmissionAssignment

---
+++ MethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class NumberMath
+    {
+        //create a method within the NumberMath class that takes in 1 or 2 numbers (optional) to perform
+        //a math operation 
+        public static int addAndMultiply(int integer1, int integer2 = 1)
+        {
+            //create a variable to store the answer of the math operation in order to return it
+            //to the main program
+            int result = Convert.ToInt32(Math.Sqrt(((integer1^2) + (integer2^2))));
+            //return the answer
+            return result;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write instructions telling the user to input 2 integers, one at a time, but make
+            //the second one optional
+            Console.WriteLine("Please input 2 integers.");
+            Console.WriteLine("First Integer:");
+            //Create variables to store user inputs
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+            //Tell user to input second variable, or leave it blank
+            Console.WriteLine("Input second integer, input not required.");
+            //Try block to see if the user inputted a second number, if so, run the method with both
+            //params
+            try
+            {
+                int secondInt = Convert.ToInt32(Console.ReadLine());
+                int ans = NumberMath.addAndMultiply(firstInt, secondInt);
+            }
+            //if only 1 int was entered, run method with only "firstInt". 
+            catch 
+            { //Call method from class1, store in int variable
+                int ans = NumberMath.addAndMultiply(firstInt);
+                //Tell user what answer is with their inputted numbers
+                Console.WriteLine("Your answer is: " + ans);
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
