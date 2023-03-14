da4429fa5a0a7ae5258acfdde465990fb834d150
markedin <119455170+markedin@users.noreply.github.com>
2023-03-14 11:43:15 -0700

    This push includes method submission assignment 2

---
+++ MethodClassSubmissionAssignment2/PerformMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment2
+{
+    internal class PerformMath
+    {
+        //Create a class with no return value (void), and pass in two integers
+        public void DoMath(int integerOne, int integerTwo)
+        {
+            //create an integer variable to store answer
+            int intOut = 0;
+            //Perform math on two passed parameters and store in intOut
+            intOut = integerOne + 5 + integerTwo * 2;
+            //Display the answer to the console
+            Console.WriteLine(intOut);
+        }
+    }
+}
---
+++ MethodClassSubmissionAssignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tell the user that you will be inputting two numbers into the method
+            Console.WriteLine("We will be passing 4 and 7 into the method.");
+            //Instantiate the class
+            PerformMath DoTheMath = new PerformMath();
+            //Pass in 4 and 7 into method by name
+            DoTheMath.DoMath(integerOne: 4, integerTwo: 7);
+
+
+            Console.ReadLine();
+        }
+    }
+}
