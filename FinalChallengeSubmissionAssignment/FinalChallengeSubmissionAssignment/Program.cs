42d780438ffc287426ad5fe28e997d68977d2b37
markedin <119455170+markedin@users.noreply.github.com>
2023-04-09 18:40:29 -0700

    This push includes the final challenge submission assignment
    This push includes the files and folder for the final challenge submission assignement

---
+++ FinalChallengeSubmissionAssignment/FinalChallengeSubmissionAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FinalChallengeSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                
+                Console.Write("Enter a First Name for the new student: ");
+                string inputFirstName = Console.ReadLine();
+                Console.Write("Enter a Last Name for the new student: ");
+                string inputLastName = Console.ReadLine();
+                Console.Write("Enter a GPA for the new student: ");
+                decimal inputGPA = Convert.ToDecimal(Console.ReadLine());
+
+                var student = new Student { FirstName = inputFirstName };
+                student.LastName = inputLastName;
+                student.GPA = inputGPA;
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from s in db.Students
+                            orderby s.Id
+                            select s;
+
+                Console.WriteLine("All students in the database: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Id + "\n" + item.FirstName + "\n" + item.LastName + "\n" + item.GPA);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+                                            
+
+            }
+            
+
+        }
+
+        public class Student
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public decimal GPA { get; set; }
+
+        }
+
+        public class StudentContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+        }
+
+    }
+}
